46f2939d93b65a25ceb75c9216873c0121f8da6f
janvi <rajeshkumari8744@gmail.com>
2025-06-30 05:24:49 +0530

    tic tac toe logic implementation

--- src/board.jsx
+++ src/board.jsx
@@ -1,22 +1,63 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Square from "./square";
 
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
+  useEffect(() => {
+    const somebodyWon = calculateWinner(squares);
+    if (somebodyWon) {
+      setWinner(somebodyWon);
+    }
+  }, [squares]);
+
+  const [winner, setWinner] = useState(null);
+
   return (
-    <div className="grid grid-cols-3 grid-rows-3">
-      {squares.map(function (square, idx) {
-        return (
-          <Square
-            onClick={function () {
-              console.log(`square ${idx + 1} clicked`);
-            }}
-            key={idx}
-          >
-            {square}
-          </Square>
-        );
-      })}
-    </div>
+    <>
+      <div className="grid grid-cols-3 grid-rows-3">
+        {squares.map(function (square, idx) {
+          return (
+            <Square
+              onClick={function () {
+                if (squares[idx]) return;
+                setSquares((prevSquares) => {
+                  prevSquares[idx] = xIsNext ? "X" : "O";
+                  return [...prevSquares]; //cloning array
+                });
+                setXIsNext(!xIsNext);
+              }}
+              key={idx}
+            >
+              {square}
+            </Square>
+          );
+        })}
+      </div>
+      {winner ? <div>{winner} won the game.</div> : null}
+    </>
   );
+}
+function calculateWinner(squares) {
+  const winningPatterns = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let winningPattern of winningPatterns) {
+    if (squares[winningPattern[0]]) {
+      if (
+        squares[winningPattern[0]] === squares[winningPattern[1]] &&
+        squares[winningPattern[0]] === squares[winningPattern[2]]
+      ) {
+        return squares[winningPattern[0]];
+      }
+    }
+  }
 }
